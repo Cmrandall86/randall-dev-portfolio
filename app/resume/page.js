66a66c34c8930feb3d2d126b6a24e7d6a9a7e2b7
Cmrandall86 <cmrandall86@gmail.com>
2024-02-07 18:38:24 -0700

    mobile styling fix

--- app/resume/page.js
+++ app/resume/page.js
@@ -6,7 +6,7 @@ import ResumePNG from "/public/Resume.png";
 
 export default function Resume() {
   return (
-    <div style={{ paddingTop: "100px" }}>
+    <div style={{ paddingTop: "50px" }}>
       <ResponsiveImage src={ResumePNG} alt="resume" customAspectRatio="150%" priority />
     </div>
   );
